a6fccab3fcb9cab3c53499941fb3ff7e0d7c8c76
dpolevodin <dpolevodin@gmail.com>
2025-02-25 12:04:47 +0600

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import {AntdRegistry} from '@ant-design/nextjs-registry';
+
+export default function RootLayout({children}: React.PropsWithChildren) {
+    return (
+        <html lang="en">
+            <body>
+                <AntdRegistry>{children}</AntdRegistry>
+            </body>
+        </html>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from 'antd';
+import '@ant-design/v5-patch-for-react-19';
+
+export default function Home() {
+    return (
+        <div className="App">
+            <Button type="primary">Button</Button>
+        </div>
+    );
+}
