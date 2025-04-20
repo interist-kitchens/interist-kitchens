a0b9147d19f6a950393fbfaf5dcc156f50db4568
Anton <drtony@yandex.ru>
2025-04-20 10:38:27 +0600

    feat: added breadcrumbs

--- src/widgets/products/ui/ProductDetail/index.tsx
+++ src/widgets/products/ui/ProductDetail/index.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import { SendOrderBtn } from '@/features/leads/sendOrder';
 import { getServerSession } from 'next-auth';
 import { authOptions, UserSession } from '@/shared/constants/authOptions';
+import { Breadcrumbs } from '@/shared/ui/Breadcrumbs';
+import { paths } from '@/shared/routing';
 
 type Props = {
     product: Product;
@@ -12,8 +14,24 @@ export const ProductDetail: FC<Props> = async ({ product }) => {
     const session: UserSession | null = await getServerSession(authOptions);
 
     return (
-        <div className={'container mx-auto px-6'}>
-            <div className={'flex flex-col lg:flex-row pt-5 pb-16 gap-8'}>
+        <div className={'container mx-auto px-6 pt-5 pb-16'}>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { title: <a href={paths.home}>Главная</a> },
+                    { title: <a href={paths.catalog}>Каталог</a> },
+                    {
+                        title: (
+                            <a
+                                href={`${paths.catalog}/${product.categories.alias}`}
+                            >
+                                {product.categories.name}
+                            </a>
+                        ),
+                    },
+                    { title: product.name },
+                ]}
+            />
+            <div className={'flex flex-col lg:flex-row gap-8 pt-5'}>
                 <ProductSlider name={product.name} images={product.images} />
                 <ProductCard
                     name={product.name}
