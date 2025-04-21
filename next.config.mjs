8c96394e3d66244d8241c752fd2bf297e257977a
Anton <drtony@yandex.ru>
2025-04-21 16:44:07 +0600

    feat: added video block

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,16 @@ const nextConfig = {
             },
         ],
     },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.(mp4|webm|ogg)$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'static/media/[name].[hash][ext]',
+            },
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
