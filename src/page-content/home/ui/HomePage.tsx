68df756256559201c47f5fda0b257c0e207ee01d
Anton <drtony@yandex.ru>
2025-04-21 12:51:16 +0600

    feat: added what included block

--- src/page-content/home/ui/HomePage.tsx
+++ src/page-content/home/ui/HomePage.tsx
@@ -1,10 +1,12 @@
 import { MainPageSlider } from '@/widgets/mainPageSlider';
 import { MainLayout } from '@/widgets/layouts';
+import { WhatIncludedBox } from '@/widgets/whatIncluded';
 
 export default async function Home() {
     return (
         <MainLayout>
             <MainPageSlider />
+            <WhatIncludedBox />
         </MainLayout>
     );
 }
---
+++ src/widgets/whatIncluded/ui/WhatIncludedBox/index.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { Title } from '@/shared/ui/Typography';
+import { WHAT_INCLUDED_CONFIG } from '@/widgets/whatIncluded/config/constants';
+import { IncludedItem } from '@/widgets/whatIncluded/ui/IncludedItem';
+
+export const WhatIncludedBox: FC = () => {
+    return (
+        <div className={'container mx-auto p-14 bg-gray-200'}>
+            <Title level={2} className={'text-2xl'}>
+                Что входит в стоимость кухни
+            </Title>
+            <div
+                className={
+                    'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-between gap-5'
+                }
+            >
+                {WHAT_INCLUDED_CONFIG.map((item) => (
+                    <IncludedItem
+                        key={item.id}
+                        image={item.image}
+                        name={item.name}
+                        description={item.description}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
